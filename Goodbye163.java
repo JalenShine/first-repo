8d26810ca22ac0826fb743a035f92c8cff302fd9
JalenShine <45207048+JalenShine@users.noreply.github.com>
2018-11-20 10:56:38 -0500

    Add files via upload

---
+++ Goodbye163.java
@@ -0,0 +1,14 @@
+/**
+  * In this code, we are simply printing out "Goodbye COMP 163."
+  * @author jdshine@aggies.ncat.edu
+  */
+
+public class Goodbye163 {
+   /**
+     * We create our main() method here so we can initialize the code.
+     * Then, we essentially print out our String.
+     */
+   public static void main(String[] args) {
+      System.out.println("Goodbye COMP 163.");
+   }
+}
